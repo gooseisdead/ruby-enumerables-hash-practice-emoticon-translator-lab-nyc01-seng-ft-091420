12c675318fe2cc720e6f3c0e0f162ef2a1f03f2e
Mike Smith <gooseisdead@mac.com>
2020-09-08 20:08:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,13 +23,13 @@ def get_japanese_emoticon(file, emoticon)
   return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning(file, emotion)
-    library = load_library(file)
-    library.each do |emotion, emoticons|
-    result = emoticons[:japanese]
-      if result == emoticon
-        return emotion
-      end
-    end
-  return "Sorry, that emoticon was not found"
-end
+# def get_english_meaning(file, emotion)
+#     library = load_library(file)
+#     library.each do |emotion, emoticons|
+#     result = emoticons[:japanese]
+#       if result == emoticon
+#         return emotion
+#       end
+#     end
+#   return "Sorry, that emoticon was not found"
+# end
