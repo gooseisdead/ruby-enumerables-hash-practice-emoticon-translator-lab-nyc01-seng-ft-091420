c8838d51b0d4ed9e66de5330562c54e3f130590e
Mike Smith <gooseisdead@mac.com>
2020-09-08 19:57:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,22 +12,32 @@ def load_library(path)
   emoticons
 end
 
-def get_japanese_emoticon(path, emoticon)
-  emoticons = load_library(path) #call load_library
-  result = emoticons["get_emoticon"][emoticon]
-  if result
-    result
-  else
-    "Sorry, that emoticon was not found"
-  end
+def get_japanese_emoticon(file, emoticon)
+    library.each do |emotion, emoticons|
+    result = emoticons[:english]
+    if result == emoticon
+      return emoticons[:japanese]
+      end
+    end
+  return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning(path, emoticon)
-  emoticons = load_library(path)
-  result = emoticons["get_meaning"][emoticon] #the same above but change get_meaning
-  if result
-    result
-  else
-    "Sorry, that emoticon was not found"
-  end
-end
+# def get_japanese_emoticon(path, emoticon)
+#   emoticons = load_library(path) #call load_library
+#   result = emoticons["get_emoticon"][emoticon]
+#   if result
+#     result
+#   else
+#     "Sorry, that emoticon was not found"
+#   end
+# end
+
+# def get_english_meaning(path, emoticon)
+#   emoticons = load_library(path)
+#   result = emoticons["get_meaning"][emoticon] #the same above but change get_meaning
+#   if result
+#     result
+#   else
+#     "Sorry, that emoticon was not found"
+#   end
+# end
