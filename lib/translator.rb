ca253aff8d063abd36047b7e4c895a120f4e2f67
Mike Smith <gooseisdead@mac.com>
2020-09-08 20:17:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_japanese_emoticon(path, emoticon)
       return value[:japanese]
       end
     end
-  return "Sorry, couldn't find that emoticon!"
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(path, emoticon)
@@ -28,5 +28,5 @@ def get_english_meaning(path, emoticon)
       return key
     end
   end
-  return "Sorry, couldn't find that emoticon!"
+  return "Sorry, that emoticon was not found"
 end
